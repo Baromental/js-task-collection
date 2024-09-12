ac2604ada8278500c1b26435589ae61e8eb59a66
Maksym <suzdalcevmaksym@gmail.com>
2024-09-12 15:59:34 +0300

    first push

---
+++ src/main.js
@@ -0,0 +1,218 @@
+// ================================================================================================================================================================================
+
+// function filterArray(numbers, value) {
+//     let newArray = [];
+//     numbers.forEach(function (number, index){ 
+//       if (number > value) {
+//         newArray.push(number)
+//       }
+//     })
+//     return newArray;
+//   }
+  
+//   console.log(filterArray([1, 2, 3, 4, 5], 3));
+//   console.log(filterArray([1, 2, 3, 4, 5], 4));
+//   console.log(filterArray([1, 2, 3, 4, 5], 5));
+//   console.log(filterArray([12, 24, 8, 41, 76], 38));
+//   console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+// ================================================================================================================================================================================
+
+// const calculateTotalPrice = (quantity, pricePerItem) => {
+//     return quantity * pricePerItem;
+//   }
+
+//   console.log(calculateTotalPrice(5, 100));
+//   console.log(calculateTotalPrice(8, 60));
+//   console.log(calculateTotalPrice(3, 400));
+
+// ================================================================================================================================================================================
+
+// const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+
+// console.log(calculateTotalPrice(5, 100));
+// console.log(calculateTotalPrice(8, 60));
+// console.log(calculateTotalPrice(3, 400));
+
+// ================================================================================================================================================================================
+
+// const calculateTotalPrice = (orderedItems) => {
+//     let totalPrice = 0;
+  
+//     orderedItems.forEach((item) => {
+//       totalPrice += item;
+//     });
+  
+//     return totalPrice;
+//   }
+
+//   console.log(calculateTotalPrice([12, 85, 37, 4]));
+//   console.log(calculateTotalPrice([164, 48, 291]));
+//   console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+  
+// ================================================================================================================================================================================
+
+// function changeEven(numbers, value) {
+//     let newArray =[];
+
+//     for (let i = 0; i < numbers.length; i += 1) {
+//       if (numbers[i] % 2 === 0) {
+//         newArray.push(numbers[i] + value);
+//       } else {
+//         newArray.push(numbers[i]);
+//       }
+//     }
+//     return newArray;
+//   }
+  
+//   console.log(changeEven([1, 2, 3, 4, 5], 10));
+//   console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
+//   console.log(changeEven([17, 24, 68, 31, 42], 100));
+//   console.log(changeEven([44, 13, 81, 92, 36, 54], 100));
+
+// ================================================================================================================================================================================
+
+// const planets = ["Earth", "Mars", "Venus", "Jupiter"];
+
+// const planetsLengths = planets.map(planet => planet.length );
+
+// console.log(planetsLengths);
+
+// ================================================================================================================================================================================
+
+// const books = [
+//     {
+//       title: "The Last Kingdom",
+//       author: "Bernard Cornwell",
+//       rating: 8.38,
+//     },
+//     {
+//       title: "Beside Still Waters",
+//       author: "Robert Sheckley",
+//       rating: 8.51,
+//     },
+//     {
+//       title: "The Dream of a Ridiculous Man",
+//       author: "Fyodor Dostoevsky",
+//       rating: 7.75,
+//     },
+//     { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+//     { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+//   ];
+  
+//   const titles = books.map( book => book.title);
+
+//   console.log(titles);
+
+// ================================================================================================================================================================================
+
+// const books = [
+//     {
+//       title: "The Last Kingdom",
+//       author: "Bernard Cornwell",
+//       genres: ["adventure", "history"],
+//     },
+//     {
+//       title: "Beside Still Waters",
+//       author: "Robert Sheckley",
+//       genres: ["fiction"],
+//     },
+//     {
+//       title: "Redder Than Blood",
+//       author: "Tanith Lee",
+//       genres: ["horror", "mysticism"],
+//     },
+//   ];
+  
+//   const genres = books.flatMap(book => book.genres);
+//   console.log(genres);
+  
+// ================================================================================================================================================================================
+
+// const users = [
+//     {
+//       name: "Moore Hensley",
+//       email: "moorehensley@indexia.com",
+//       eyeColor: "blue",
+//       friends: ["Sharron Pace"],
+//       isActive: false,
+//       balance: 2811,
+//       skills: ["ipsum", "lorem"],
+//       gender: "male",
+//       age: 37,
+//     },
+//     {
+//       name: "Sharlene Bush",
+//       email: "sharlenebush@tubesys.com",
+//       eyeColor: "blue",
+//       friends: ["Briana Decker", "Sharron Pace"],
+//       isActive: true,
+//       balance: 3821,
+//       skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+//       gender: "female",
+//       age: 34,
+//     },
+//     {
+//       name: "Ross Vazquez",
+//       email: "rossvazquez@xinware.com",
+//       eyeColor: "green",
+//       friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//       isActive: false,
+//       balance: 3793,
+//       skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+//       gender: "male",
+//       age: 24,
+//     },
+//     {
+//       name: "Elma Head",
+//       email: "elmahead@omatom.com",
+//       eyeColor: "green",
+//       friends: ["Goldie Gentry", "Aisha Tran"],
+//       isActive: true,
+//       balance: 2278,
+//       skills: ["adipisicing", "irure", "velit"],
+//       gender: "female",
+//       age: 21,
+//     },
+//     {
+//       name: "Carey Barr",
+//       email: "careybarr@nurali.com",
+//       eyeColor: "blue",
+//       friends: ["Jordan Sampson", "Eddie Strong"],
+//       isActive: true,
+//       balance: 3951,
+//       skills: ["ex", "culpa", "nostrud"],
+//       gender: "male",
+//       age: 27,
+//     },
+//     {
+//       name: "Blackburn Dotson",
+//       email: "blackburndotson@furnigeer.com",
+//       eyeColor: "brown",
+//       friends: ["Jacklyn Lucas", "Linda Chapman"],
+//       isActive: false,
+//       balance: 1498,
+//       skills: ["non", "amet", "ipsum"],
+//       gender: "male",
+//       age: 38,
+//     },
+//     {
+//       name: "Sheree Anthony",
+//       email: "shereeanthony@kog.com",
+//       eyeColor: "brown",
+//       friends: ["Goldie Gentry", "Briana Decker"],
+//       isActive: true,
+//       balance: 2764,
+//       skills: ["lorem", "veniam", "culpa"],
+//       gender: "female",
+//       age: 39,
+//     },
+//   ];
+
+// const getUserEmails = (users) => {
+//     return users.map(user => user.email)
+//   };
+
+// console.log(getUserEmails(users));
+
+// ================================================================================================================================================================================
